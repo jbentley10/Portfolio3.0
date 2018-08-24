1a9f756a18274e6f15f7664bd5cafbdb02a08175
John Bentley <jbentley@tripwire.com>
2018-08-24 09:36:45 -0700

    Initial commit.

---
+++ scripts/portfolio-variables.js
@@ -0,0 +1,208 @@
+/*jshint multistr: true */
+var tractorTipping = '<div class=\'work-pictures\'>\
+  <img class=\'tractor-tipping-1\' src=\'images/explore/tractor-tipping-menu.png\'>\
+</div>\
+<div class=\'work-pictures\'>\
+  <img class=\'tractor-tipping-2\' src=\'images/explore/tractor-tipping/2.png\'>\
+</div>\
+<div class=\'work-pictures\'>\
+  <img class=\'tractor-tipping-3\' src=\'images/explore/tractor-tipping/3.png\'>\
+</div>\
+<div class=\'work-pictures\'>\
+  <img class=\'tractor-tipping-4\' src=\'images/explore/tractor-tipping/4.png\'>\
+</div>\
+<div class=\'work-pictures\'>\
+  <img class=\'tractor-tipping-5\' src=\'images/explore/tractor-tipping/5.png\'>\
+</div>';
+
+var projectWarp = '<div class=\'work-pictures\'>\
+  <img class=\'warp-1\' src=\'images/explore/warp-menu.png\'>\
+</div>\
+<div class=\'work-pictures\'>\
+  <img class=\'warp-2\' src=\'images/explore/warp/2.png\'>\
+</div>\
+<div class=\'work-pictures\'>\
+  <img class=\'warp-3\' src=\'images/explore/warp/3.png\'>\
+</div>\
+<div class=\'work-pictures\'>\
+  <img class=\'warp-4\' src=\'images/explore/warp/4.png\'>\
+</div>\
+<div class=\'work-pictures\'>\
+  <img class=\'warp-5\' src=\'images/explore/warp/5.png\'>\
+</div>';
+
+var cocooned = '<div class=\'work-pictures\'>\
+  <img class=\'cocooned-1\' src=\'images/explore/cocooned/1.png\'>\
+</div>\
+<div class=\'work-pictures\'>\
+  <img class=\'cocooned-2\' src=\'images/explore/cocooned/2.png\'>\
+</div>\
+<div class=\'work-pictures\'>\
+  <img class=\'cocooned-3\' src=\'images/explore/cocooned/3.png\'>\
+</div>\
+<div class=\'work-pictures\'>\
+  <img class=\'cocooned-4\' src=\'images/explore/cocooned/4.png\'>\
+</div>\
+<div class=\'work-pictures\'>\
+  <img class=\'cocooned-5\' src=\'images/explore/cocooned/5.png\'>\
+</div>\
+<div class=\'work-pictures\'>\
+  <img class=\'cocooned-6\' src=\'images/explore/cocooned/6.png\'>\
+</div>\
+<div class=\'work-pictures\'>\
+  <img class=\'cocooned-7\' src=\'images/explore/cocooned/7.png\'>\
+</div>\
+<div class=\'work-pictures\'>\
+  <img class=\'cocooned-8\' src=\'images/explore/cocooned/8.png\'>\
+</div>\
+<div class=\'work-pictures\'>\
+  <img class=\'cocooned-9\' src=\'images/explore/cocooned/9.png\'>\
+</div>';
+
+var avChart = '<div class=\'work-pictures\'>\
+  <img class=\'av-charts-1\' src=\'images/explore/av-chart.png\'>\
+</div>';
+
+var theSpot = '<div class=\'work-pictures\'>\
+  <img class=\'the-spot-1\' src=\'images/explore/the-spot/1.png\'>\
+</div>\
+<div class=\'work-pictures\'>\
+  <img class=\'the-spot-2\' src=\'images/explore/the-spot/2.png\'>\
+</div>\
+<div class=\'work-pictures\'>\
+  <img class=\'the-spot-3\' src=\'images/explore/the-spot/3.png\'>\
+</div>\
+<div class=\'work-pictures\'>\
+  <img class=\'the-spot-4\' src=\'images/explore/the-spot/4.png\'>\
+</div>\
+<div class=\'work-pictures\'>\
+  <img class=\'the-spot-5\' src=\'images/explore/the-spot/5.png\'>\
+</div>\
+<div class=\'work-pictures\'>\
+  <img class=\'the-spot-6\' src=\'images/explore/the-spot/6.png\'>\
+</div>\
+<div class=\'work-pictures\'>\
+  <img class=\'the-spot-7\' src=\'images/explore/the-spot/7.png\'>\
+</div>';
+
+var idApp = '<div class=\'work-pictures\'>\
+  <img class=\'id-app-1\' src=\'images/explore/id-app/1.png\'>\
+</div>\
+<div class=\'work-pictures\'>\
+  <img class=\'id-app-2\' src=\'images/explore/id-app/2.png\'>\
+</div>\
+<div class=\'work-pictures\'>\
+  <img class=\'id-app-3\' src=\'images/explore/id-app/3.png\'>\
+</div>\
+<div class=\'work-pictures\'>\
+  <img class=\'id-app-4\' src=\'images/explore/id-app/4.png\'>\
+</div>\
+<div class=\'work-pictures\'>\
+  <img class=\'id-app-5\' src=\'images/explore/id-app/5.png\'>\
+</div>';
+
+var gamePlan = '<div class=\'work-pictures\'>\
+  <img class=\'game-plan-1\' src=\'images/explore/game-plan/1.png\'>\
+</div>\
+<div class=\'work-pictures\'>\
+  <img class=\'game-plan-2\' src=\'images/explore/game-plan/2.png\'>\
+</div>\
+<div class=\'work-pictures\'>\
+  <img class=\'game-plan-3\' src=\'images/explore/game-plan/3.png\'>\
+</div>\
+<div class=\'work-pictures\'>\
+  <img class=\'game-plan-4\' src=\'images/explore/game-plan/4.png\'>\
+</div>';
+
+var upsell = '<div class=\'work-pictures\'>\
+  <img class=\'upsell-1\' src=\'images/explore/upsell/old/1.png\'>\
+</div>\
+<div class=\'work-pictures\'>\
+  <img class=\'upsell-2\' src=\'images/explore/upsell/old/2.png\'>\
+</div>\
+<div class=\'work-pictures\'>\
+  <img class=\'upsell-3\' src=\'images/explore/upsell/new/3.png\'>\
+</div>\
+<div class=\'work-pictures\'>\
+  <img class=\'upsell-4\' src=\'images/explore/upsell/new/4.png\'>\
+</div>';
+
+var styleGuide = '<div class=\'work-pictures\'>\
+  <img class=\'style-guide-1\' src=\'images/explore/style-guide/1.png\'>\
+</div>\
+<div class=\'work-pictures\'>\
+  <img class=\'style-guide-2\' src=\'images/explore/style-guide/2.png\'>\
+</div>\
+<div class=\'work-pictures\'>\
+  <img class=\'style-guide-3\' src=\'images/explore/style-guide/3.png\'>\
+</div>\
+<div class=\'work-pictures\'>\
+  <img class=\'style-guide-4\' src=\'images/explore/style-guide/4.png\'>\
+</div>';
+
+var multiDaySessions = '<div class=\'work-pictures\'>\
+  <img class=\'multi-day-sessions-1\' src=\'images/explore/multi-day-sessions.jpg\'>\
+</div>';
+
+var singleDaySessions = '<div class=\'work-pictures\'>\
+  <img class=\'single-day-sessions-1\' src=\'images/explore/single-day-sessions.png\'>\
+</div>';
+
+var welcomePacket = '<div class=\'work-pictures\'>\
+  <img class=\'welcome-packet-1\' src=\'images/explore/welcome-packet/1.png\'>\
+</div>\
+<div class=\'work-pictures\'>\
+  <img class=\'welcome-packet-2\' src=\'images/explore/welcome-packet/2.png\'>\
+</div>\
+<div class=\'work-pictures\'>\
+  <img class=\'welcome-packet-3\' src=\'images/explore/welcome-packet/3.png\'>\
+</div>';
+
+var avCharts = '<div class=\'work-pictures\'>\
+  <img class=\'av-charts-1\' src=\'images/explore/av-charts/1.png\'>\
+</div>';
+
+var staffCorner = '<div class=\'work-pictures\'>\
+  <img class=\'staff-corner-1\' src=\'images/explore/staff-corner/log-in.png\'>\
+</div>\
+<div class=\'work-pictures\'>\
+  <img class=\'staff-corner-2\' src=\'images/explore/staff-corner/staff-tasks.png\'>\
+</div>\
+<div class=\'work-pictures\'>\
+  <img class=\'staff-corner-3\' src=\'images/explore/staff-corner/help-desk.png\'>\
+</div>\
+<div class=\'work-pictures\'>\
+  <img class=\'staff-corner-4\' src=\'images/explore/staff-corner/videos.png\'>\
+</div>';
+
+var next = '<div class=\'work-pictures\'>\
+  <img class=\'next-1\' src=\'images/explore/next/1.png\'>\
+</div>\
+<div class=\'work-pictures\'>\
+  <img class=\'next-2\' src=\'images/explore/next/2.png\'>\
+</div>\
+<div class=\'work-pictures\'>\
+  <img class=\'next-3\' src=\'images/explore/next/3.png\'>\
+</div>';
+
+var healthAndSafety = '<div class=\'work-pictures\'>\
+  <img class=\'health-and-safety-1\' src=\'images/explore/health-and-safety/1.png\'>\
+</div>\
+<div class=\'work-pictures\'>\
+  <img class=\'health-and-safety-2\' src=\'images/explore/health-and-safety/2.png\'>\
+</div>\
+<div class=\'work-pictures\'>\
+  <img class=\'health-and-safety-3\' src=\'images/explore/health-and-safety/3.png\'>\
+</div>\
+<div class=\'work-pictures\'>\
+  <img class=\'health-and-safety-4\' src=\'images/explore/health-and-safety/4.png\'>\
+</div>\
+<div class=\'work-pictures\'>\
+  <img class=\'health-and-safety-5\' src=\'images/explore/health-and-safety/5.png\'>\
+</div>\
+<div class=\'work-pictures\'>\
+  <img class=\'health-and-safety-6\' src=\'images/explore/health-and-safety/6.png\'>\
+</div>\
+<div class=\'work-pictures\'>\
+  <img class=\'health-and-safety-7\' src=\'images/explore/health-and-safety/7.png\'>\
+</div>';
